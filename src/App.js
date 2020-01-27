86f0f2e3b4503db16f91b81ada456deb03ccdb2d
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-01-27 15:41:41 +0200

    new syntax new attempt of project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Route, Switch} from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Route exact path="/" component={"Home"}/>
-    <Route path="/about" component={"About"}/>
-    <Route path="/contact" component={"Contact"}/>
     <Navigation />
+    <Switch>
+      <Route exact path ="/">
+        <Home/>
+      </Route>
+      <Route path="/about">
+      <About/>
+      </Route>
+      <Route path="/contact">
+        <Contact/>
+      </Route>
+    </Switch>
   </div>
 );
 
 export default App;
-
-
-
